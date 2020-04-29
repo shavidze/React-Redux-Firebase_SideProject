31ea8fd2b70065c8a47be1a4abdf3de1e567478a
shavidze <sabashavidze@gmail.com>
2020-04-29 06:08:14 -0700

    prject list

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,34 +2,28 @@ import React from "react";
 
 export const ProjectList = () => {
   return (
-    <div>
-      <div className="project-list section">
-        <div className="card z-depth0 projectsummary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted By Saba</p>
-            <p className="grey-text">3rd September</p>
-          </div>
+    <div className="project-list section">
+      <div className="card z-depth0 projectsummary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Saba</p>
+          <p className="grey-text">3rd September</p>
         </div>
       </div>
 
-      <div className="project-list section">
-        <div className="card z-depth0 projectsummary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted By Saba</p>
-            <p className="grey-text">3rd September</p>
-          </div>
+      <div className="card z-depth0 projectsummary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Saba</p>
+          <p className="grey-text">3rd September</p>
         </div>
       </div>
 
-      <div className="project-list section">
-        <div className="card z-depth0 projectsummary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted By Saba</p>
-            <p className="grey-text">3rd September</p>
-          </div>
+      <div className="card z-depth0 projectsummary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted By Saba</p>
+          <p className="grey-text">3rd September</p>
         </div>
       </div>
     </div>
