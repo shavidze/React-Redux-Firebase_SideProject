2476113b3f74affa85d4ffc47900825a61393885
shavidze <sabashavidze@gmail.com>
2020-05-11 11:10:40 -0700

    firebase security rules

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -2,15 +2,18 @@ import React from "react";
 import moment from "moment";
 
 export const ProjectSummary = ({ project }) => {
+  console.log("proj", project);
 
-  console.log("proj",project);
-  
   return (
     <div className="card z-depth0 projectsummary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-  <p>Posted By {project.authorFirstName} {project.authorLastName}</p>
-  <p className="grey-text">{project.authorFirstName}</p>
+        <p>
+          Posted By {project.authorFirstName} {project.authorLastName}
+        </p>
+        <p className="grey-text">
+          {moment(project.createdAt.seconds).format("DD/MM/YYYY")}
+        </p>
       </div>
     </div>
   );
