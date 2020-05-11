e3f689fcad0e8a53978543bcf963e65a681e5282
shavidze <sabashavidze@gmail.com>
2020-05-11 12:32:14 -0700

    deploying

---
+++ dist/precache-manifest.57999de467b439d91bb25949edd34ac3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebf4ca7844c26fd35e005812862cdfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9825ee6d52c40099295e",
+    "url": "/static/css/main.421f29e9.chunk.css"
+  },
+  {
+    "revision": "7df334de1a9d68fb45df",
+    "url": "/static/js/2.b56a345b.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.b56a345b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9825ee6d52c40099295e",
+    "url": "/static/js/main.a9157ed0.chunk.js"
+  },
+  {
+    "revision": "36a205d7f90c8f5512b9",
+    "url": "/static/js/runtime-main.bc11e9d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
